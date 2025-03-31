be2b6dc4c5a0f4f5d2c109ef04522052d1af3c3c
yjr <emilyryoo5307@gmail.com>
2025-03-31 13:57:48 +0900

    initial project

---
+++ src/main/java/com/solution/Ongi/OngiApplication.java
@@ -0,0 +1,13 @@
+package com.solution.Ongi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solution/Ongi/OngiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solution.Ongi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
