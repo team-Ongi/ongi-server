d91511f8ce96b16282f9adf32464d23fa04e04c7
suuu0719 <suuu0719@gmail.com>
2025-05-11 19:16:43 +0900

    fix: JAR 환경에서 firebase 키 로드 오류 해결을 위해 ClassPathResource로 수정

--- src/main/java/com/solution/Ongi/infra/FirebaseConfig.java
+++ src/main/java/com/solution/Ongi/infra/FirebaseConfig.java
@@ -3,26 +3,25 @@ package com.solution.Ongi.infra;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-
-import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
-import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
     public void init() throws IOException {
-        FileInputStream serviceAccount=new FileInputStream(
-                new ClassPathResource("firebaseAccountKey.json").getFile());
-        FirebaseOptions options=FirebaseOptions.builder()
+        ClassPathResource resource = new ClassPathResource("firebaseAccountKey.json");
+        try (InputStream serviceAccount = resource.getInputStream()) {
+            FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
-        if(FirebaseApp.getApps().isEmpty()){
-            FirebaseApp.initializeApp(options);
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
         }
-
     }
 }
