a5448cd8e56098b8937eb36426057e0e2b86ea60
yjr <emilyryoo5307@gmail.com>
2025-07-03 03:30:05 +0900

    release2.0: test 주석 처리

--- src/test/java/com/solution/Ongi/OngiApplicationTests.java
+++ src/test/java/com/solution/Ongi/OngiApplicationTests.java
@@ -1,13 +1,15 @@
-package com.solution.Ongi;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class OngiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.solution.Ongi;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//@SpringBootTest
+//@ActiveProfiles("test")
+//class OngiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
