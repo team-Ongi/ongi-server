80c9010f2452946c8ba454d836fb1b1d66091a4c
yjr <emilyryoo5307@gmail.com>
2025-07-10 03:38:51 +0900

    fix: firebase 파일경로화

--- src/main/java/com/solution/Ongi/infra/firebase/FirebaseProperties.java
+++ src/main/java/com/solution/Ongi/infra/firebase/FirebaseProperties.java
@@ -1,23 +1,17 @@
 package com.solution.Ongi.infra.firebase;
 
-import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @ConfigurationProperties(prefix = "firebase")
-public record FirebaseProperties(
-        String type,
-        String projectId,
-        String privateKeyId,
-        String privateKey,
-        String clientEmail,
-        String clientId,
-        String authUri,
-        String tokenUri,
-        String authProviderX509CertUrl,
-        String clientX509CertUrl,
-        String universeDomain
-) { public String privateKey() {
+public class FirebaseProperties {
 
-        return privateKey.replace("\\n", "\n");
+    private String serviceAccountFile;
+
+    public String getServiceAccountFile() {
+        return serviceAccountFile;
+    }
+    public void setServiceAccountFile(String serviceAccountFile) {
+        this.serviceAccountFile = serviceAccountFile;
     }
 }
