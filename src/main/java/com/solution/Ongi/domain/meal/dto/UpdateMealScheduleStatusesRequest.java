9507f64db3648038bd49c1adb61306c79d50a68d
yjr <110809579+YEJIRYOO@users.noreply.github.com>
2025-07-19 18:34:59 +0900

    refactor: updateMealScheduleStatusesResquest 레코드 업데이트

--- src/main/java/com/solution/Ongi/domain/meal/dto/UpdateMealScheduleStatusesRequest.java
+++ src/main/java/com/solution/Ongi/domain/meal/dto/UpdateMealScheduleStatusesRequest.java
@@ -1,11 +1,6 @@
 package com.solution.Ongi.domain.meal.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-public class UpdateMealScheduleStatusesRequest {
-    private Long scheduleId;
-    private boolean status;
-}
+public record UpdateMealScheduleStatusesRequest(
+    Long scheduleId,
+    boolean status
+){}
